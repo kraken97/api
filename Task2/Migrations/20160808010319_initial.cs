5a57f104f5d78718b77b29a824be4b88feb7be96
Dima Zherebko <kraken@live.ru>
2016-08-09 17:31:07 +0300

    fixes

---
+++ Task2/Migrations/20160808010319_initial.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Task2.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pages",
+                columns: table => new
+                {
+                    PageId = table.Column<int>(nullable: false)
+                        .Annotation("Autoincrement", true),
+                    AddedDate = table.Column<DateTime>(nullable: false, defaultValueSql: "strftime('%Y-%m-%d %H:%M:%S')"),
+                    Content = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(maxLength: 200, nullable: true),
+                    Title = table.Column<string>(maxLength: 50, nullable: true),
+                    UrlName = table.Column<string>(maxLength: 15, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pages", x => x.PageId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NavLinks",
+                columns: table => new
+                {
+                    NavLinkId = table.Column<int>(nullable: false)
+                        .Annotation("Autoincrement", true),
+                    PageId = table.Column<int>(nullable: true),
+                    ParentLinkID = table.Column<int>(nullable: true),
+                    Position = table.Column<int>(nullable: true),
+                    Title = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NavLinks", x => x.NavLinkId);
+                    table.ForeignKey(
+                        name: "FK_NavLinks_Pages_PageId",
+                        column: x => x.PageId,
+                        principalTable: "Pages",
+                        principalColumn: "PageId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RelatedPages",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Autoincrement", true),
+                    Page1Id = table.Column<int>(nullable: true),
+                    Page2Id = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RelatedPages", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_RelatedPages_Pages_Page1Id",
+                        column: x => x.Page1Id,
+                        principalTable: "Pages",
+                        principalColumn: "PageId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_RelatedPages_Pages_Page2Id",
+                        column: x => x.Page2Id,
+                        principalTable: "Pages",
+                        principalColumn: "PageId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NavLinks_PageId",
+                table: "NavLinks",
+                column: "PageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RelatedPages_Page1Id",
+                table: "RelatedPages",
+                column: "Page1Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RelatedPages_Page2Id",
+                table: "RelatedPages",
+                column: "Page2Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NavLinks");
+
+            migrationBuilder.DropTable(
+                name: "RelatedPages");
+
+            migrationBuilder.DropTable(
+                name: "Pages");
+        }
+    }
+}
---
+++ Task2/Migrations/SqliteContextModelSnapshot.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Task2.Models;
+
+namespace Task2.Migrations
+{
+    [DbContext(typeof(SqliteContext))]
+    partial class SqliteContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
+
+            modelBuilder.Entity("Task2.Models.NavLink", b =>
+                {
+                    b.Property<int>("NavLinkId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int?>("PageId");
+
+                    b.Property<int?>("ParentLinkID");
+
+                    b.Property<int?>("Position");
+
+                    b.Property<string>("Title")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 50);
+
+                    b.HasKey("NavLinkId");
+
+                    b.HasIndex("PageId");
+
+                    b.ToTable("NavLinks");
+                });
+
+            modelBuilder.Entity("Task2.Models.Page", b =>
+                {
+                    b.Property<int>("PageId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("AddedDate")
+                        .ValueGeneratedOnAdd()
+                        .HasDefaultValueSql("strftime('%Y-%m-%d %H:%M:%S')");
+
+                    b.Property<string>("Content")
+                        .IsRequired();
+
+                    b.Property<string>("Description")
+                        .HasAnnotation("MaxLength", 200);
+
+                    b.Property<string>("Title")
+                        .HasAnnotation("MaxLength", 50);
+
+                    b.Property<string>("UrlName")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 15);
+
+                    b.HasKey("PageId");
+
+                    b.ToTable("Pages");
+                });
+
+            modelBuilder.Entity("Task2.Models.RelatedPages", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int?>("Page1Id");
+
+                    b.Property<int?>("Page2Id");
+
+                    b.HasKey("ID");
+
+                    b.HasIndex("Page1Id");
+
+                    b.HasIndex("Page2Id");
+
+                    b.ToTable("RelatedPages");
+                });
+
+            modelBuilder.Entity("Task2.Models.NavLink", b =>
+                {
+                    b.HasOne("Task2.Models.Page", "Page")
+                        .WithMany()
+                        .HasForeignKey("PageId");
+                });
+
+            modelBuilder.Entity("Task2.Models.RelatedPages", b =>
+                {
+                    b.HasOne("Task2.Models.Page", "Page1")
+                        .WithMany()
+                        .HasForeignKey("Page1Id");
+
+                    b.HasOne("Task2.Models.Page", "Page2")
+                        .WithMany()
+                        .HasForeignKey("Page2Id");
+                });
+        }
+    }
+}
